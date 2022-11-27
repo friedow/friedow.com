f74a869a4563b46155d2feae7e4d7063244ea32e
friedow <christian@friedow.com>
2022-11-27 18:19:14 +0100

    refactor: remove unused images

--- website/.vitepress/config.js
+++ website/.vitepress/config.js
@@ -1,6 +1,5 @@
 export default {
   head: [
-    // ["link", { rel: "apple-touch-icon", sizes: "180x180", href: "/favicons/apple-touch-icon.png", },],
     [
       "link",
       { rel: "icon", type: "image/png", sizes: "32x32", href: "/favicon.png" },
@@ -9,11 +8,9 @@ export default {
       "link",
       { rel: "icon", type: "image/png", sizes: "16x16", href: "/favicon.png" },
     ],
-    // ["link", { rel: "manifest", href: "/favicons/site.webmanifest" }], ["link", { rel: "mask-icon", href: "/favicons/safari-pinned-tab.svg", color: "#5bbad5", },],
     ["link", { rel: "shortcut icon", href: "/favicon.ico" }],
-    // ["meta", { name: "msapplication-TileColor", content: "#2b5797" }],
-    // ["meta", { name: "msapplication-config", content: "/favicons/browserconfig.xml" },],
-    // ["meta", { name: "theme-color", content: "#ffffff" }],
+    ["meta", { name: "msapplication-TileColor", content: "#d69145" }],
+    ["meta", { name: "theme-color", content: "#d69145" }],
   ],
   title: "friedow",
   description: "custom-made code",
